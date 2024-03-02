e1ecc353ea237e418ce3622daf62be59e492e756
Prajeesh <prajeeshrajan07@gmail.com>
2024-03-02 14:13:45 +0530

    Navbar comp  initilized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Route,Link, } from 'react-router-dom';
+import { Layout,Space,Typography } from 'antd';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+        <div className='navbar'>
+
+        </div>
+        <div className='main'>
+hello
+        </div>
+        <div className='footer'>
+
+        </div>
+
+    </div>
   )
 }
 
