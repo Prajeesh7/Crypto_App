83d3f2c70a0cc9b717731ee001153cab8fb3affe
Prajeesh <prajeeshrajan07@gmail.com>
2024-03-03 16:15:29 +0530

    All components imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
-import { Layout, Space, Typography } from "antd";
+import { Layout, Space, Switch, Typography } from "antd";
 import "./App.css";
 
-import { Navbar } from "./Component";
+import {
+  Navbar,
+  CryptoDetails,
+  Cryptocurrencies,
+  Exchange,
+  Homepage,
+  News,
+} from "./Component";
 
 const App = () => {
   return (
@@ -11,7 +18,29 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchange">
+                <Exchange />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
